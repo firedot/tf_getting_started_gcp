be743a1f56f78571d75dbd5c903d830c4e1d8e32
Aleksandar Todoro <linked2u@gmail.com>
2020-05-25 11:45:54 +0300

    changed the image which the VM Instance uses

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "google_compute_instance" "vm_instance" {
   tags         = ["web", "dev"]
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
     }
   }
   network_interface {
